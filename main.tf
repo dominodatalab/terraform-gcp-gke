8eded1953e1c554796890c4163ef10f107c0ab55
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-09 17:21:27 -0700

    Avoid master cidr collisions

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "google_container_cluster" "domino_cluster" {
   private_cluster_config {
     enable_private_endpoint = true
     enable_private_nodes    = true
-    master_ipv4_cidr_block  = "10.1.0.0/28"
+    master_ipv4_cidr_block  = "172.16.1.0/28"
   }
 
   ip_allocation_policy {}
