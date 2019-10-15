57312f84268f114a913347f2a74738bf4d3ab57e
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-14 18:39:59 -0700

    Allow for Configurable Node Pool SSD Sizes

--- variables.tf
+++ variables.tf
@@ -22,6 +22,11 @@ variable "build_nodes_preemptible" {
   default = true
 }
 
+variable "build_nodes_ssd_gb" {
+  type    = number
+  default = 100
+}
+
 variable "build_node_type" {
   type    = string
   default = "n1-standard-1"
@@ -57,6 +62,11 @@ variable "compute_nodes_min" {
 variable "compute_nodes_preemptible" {
   type    = bool
   default = true
+}
+
+variable "compute_nodes_ssd_gb" {
+  type    = number
+  default = 100
 }
 
 variable "compute_node_type" {
@@ -97,9 +107,14 @@ variable "platform_nodes_preemptible" {
   default = true
 }
 
+variable "platform_nodes_ssd_gb" {
+  type    = number
+  default = 100
+}
+
 variable "platform_node_type" {
   type    = string
-  default = "n1-standard-8"
+  default = "n1-standard-2"
 }
 
 variable "enable_vertical_pod_autoscaling" {
