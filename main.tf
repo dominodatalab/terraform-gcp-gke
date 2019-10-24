059b8b3210f4aedf72d98882e6fe6751316b7d25
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-24 13:02:49 -0700

    fmt fix

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "gcs" {
     bucket = "domino-terraform-default" # Should specify using cli -backend-config="bucket=domino-terraform-default"
     # Override with `terraform init -backend-config="prefix=/terraform/state/${TF_VAR_cluster_name}"`
-    prefix  = "terraform/state"
+    prefix = "terraform/state"
   }
 }
 
