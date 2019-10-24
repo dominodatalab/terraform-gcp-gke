24fbd6e38c641fa3118deb1578f62e91cf66a461
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-24 15:54:51 -0700

    fix bugs

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
+output "cluster" {
+  value = {
+    "client_certificate": google_container_cluster.domino_cluster.master_auth[0].client_certificate,
+    "client_key": google_container_cluster.domino_cluster.master_auth[0].client_key,
+    "cluster_ca_certificate": google_container_cluster.domino_cluster.master_auth[0].cluster_ca_certificate,
+    "cluster_ipv4_cidr": google_container_cluster.domino_cluster.cluster_ipv4_cidr,
+    "name": google_container_cluster.domino_cluster.name
+    "public_endpoint": google_container_cluster.domino_cluster.private_cluster_config[0].public_endpoint
+  }
+  description = "GKE cluster information"
+}
+
 output "dns" {
   value       = google_dns_record_set.a.name
   description = "The external (public) DNS name for the Domino UI"
@@ -6,17 +18,6 @@ output "dns" {
 output "static_ip" {
   value       = google_compute_address.static_ip.address
   description = "The external (public) static IPv4 for the Domino UI"
-}
-
-output "cluster" {
-  value = {
-    "client_certificate": google_container_cluster.domino_cluster.master_auth[0].client_certificate,
-    "client_key": google_container_cluster.domino_cluster.master_auth[0].client_key,
-    "cluster_ca_certificate": google_container_cluster.domino_cluster.master_auth[0].client_ca_certificate,
-    "cluster_ipv4_cidr": google_container_cluster.domino_cluster.cluster_ipv4_cidr,
-    "name": google_container_cluster.domino_cluster.name
-    "public_endpoint": google_container_cluster.domino_cluster.private_cluster_config.public_endpoint
-  }
 }
 
 output "google_filestore_instance" {
