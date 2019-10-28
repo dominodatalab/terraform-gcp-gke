289b97c7d054a77cf67c52cee4af43ee3cd89853
Saahil Dhulla <saahildhulla@Saahils-MacBook-Pro.local>
2019-10-28 16:42:59 -0700

    fixing resource name

--- service-accounts.tf
+++ service-accounts.tf
@@ -59,7 +59,7 @@ resource "google_project_iam_member" "platform_object_admin" {
   member  = "serviceAccount:${google_service_account.platform.email}"
 }
 
-resource "google_project_iam_member" "platform_object_admin" {
+resource "google_project_iam_member" "platform_workload_identity_user" {
   project = var.project
   role    = "roles/iam.workloadIdentityUser"
   member  = "serviceAccount:${google_service_account.platform.email}"
