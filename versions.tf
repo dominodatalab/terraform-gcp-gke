e4d9b251a4ddf0d11522570bf7f8b8e6ce85e96c
Raymond Etornam <56278673+ddl-retornam@users.noreply.github.com>
2019-10-29 14:17:32 -0700

    pr feedback

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    google      = ">=2.17.0"
-    google-beta = ">=2.17.0"
+    google      = ">=2.18.1"
+    google-beta = ">=2.18.1"
   }
 }
