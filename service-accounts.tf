9a36bb9c620e6a26e5443698ea62c04750675026
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2020-01-07 17:11:43 -0800

    remove docker-registry service account binding
    In the past we'd discussed having this binding implicit based on the
    namespace, but the current implementation in the installer requires the
    SA reference:
    https://github.com/cerebrotech/platform-apps/blob/ea635b2eb8ff485a79850c6df69f04e8c1493314/src/fleetcommand_agent/installer/config/types.py#L65

--- service-accounts.tf
+++ service-accounts.tf
@@ -76,11 +76,3 @@ resource "google_project_iam_member" "platform_monitoring" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.platform.email}"
 }
-
-resource "google_service_account_iam_binding" "platform_docker_registry" {
-  service_account_id = google_service_account.platform.name
-  role               = "roles/iam.workloadIdentityUser"
-  members = [
-    "serviceAccount:${var.project}.svc.id.goog[${var.platform_namespace}/docker-registry]",
-  ]
-}
