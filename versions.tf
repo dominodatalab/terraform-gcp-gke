c2fef179efd42648126abf5b397c9e2fb57c7cea
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2020-05-16 13:30:34 -0700

    Update Provider Versions

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    google      = ">=3.10.0"
-    google-beta = ">=3.10.0"
-    http        = ">=1.1.1"
-    kubernetes  = "~> 1.10.0"
+    google      = ">=3.21.0"
+    google-beta = ">=3.21.0"
+    http        = ">=1.2.0"
+    kubernetes  = "~> 1.11.2"
     local       = ">=1.4.0"
-    random      = ">=2.2"
+    random      = ">=2.2.1"
   }
 }
