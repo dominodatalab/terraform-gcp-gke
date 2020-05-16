549eacceca34051bba6a1716833b8533d12989db
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2020-05-16 15:51:24 -0700

    fmt

--- main.tf
+++ main.tf
@@ -131,6 +131,12 @@ resource "google_container_cluster" "domino_cluster" {
     channel = var.gke_release_channel
   }
 
+  lifecycle {
+    ignore_changes = [
+      node_config
+    ]
+  }
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
@@ -138,6 +144,9 @@ resource "google_container_cluster" "domino_cluster" {
 
   # Workaround for https://github.com/terraform-providers/terraform-provider-google/issues/3385
   initial_node_count = max(1, var.compute_nodes_min) + max(0, var.gpu_nodes_min) + var.platform_nodes_max
+  node_config {
+    service_account = google_service_account.gke.name
+  }
 
   network    = google_compute_network.vpc_network.self_link
   subnetwork = google_compute_subnetwork.default.self_link
