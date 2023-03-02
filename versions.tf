67100cc1969dd95090702d5fc1e7863c8f7e26c1
Michael Fraenkel <michael.fraenkel@dominodatalab.com>
2023-03-01 19:30:11 -0700

    Revert "PLAT-6081: Generate unique keyring/keys"
    This reverts commit 3a44860d03429c97bed6326a6cdf4bcda7b822c7.

--- versions.tf
+++ versions.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 4.0, < 5.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
