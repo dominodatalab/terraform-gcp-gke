d5cc1cc3de432ba686e01f89ed97b96a7f320ecd
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2024-07-10 18:06:57 -0700

    Add comment about the weird restart

--- templates/nfs-install.sh
+++ templates/nfs-install.sh
@@ -8,5 +8,6 @@ mount ${nfs_path}
 chmod 777 ${nfs_path}
 echo '${nfs_path} 10.0.0.0/255.0.0.0(rw,async,no_root_squash)' >> /etc/exports
 systemctl enable nfs-kernel-server --now
+# without this restart, exports isn't respected for some reason
 sleep 5
 /etc/init.d/nfs-kernel-server restart
