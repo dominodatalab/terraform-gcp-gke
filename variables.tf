7f9b4b0d3674c992b0038321613f9c226f64b756
Secretions <joaquin.lopez@dominodatalab.com>
2024-08-19 13:45:21 -0700

    PLAT-8513: Update default kubernetes version to 1.30 (#120)

--- variables.tf
+++ variables.tf
@@ -136,7 +136,7 @@ variable "gke" {
   EOF
 
   type = object({
-    k8s_version     = optional(string, "1.29"),
+    k8s_version     = optional(string, "1.30"),
     release_channel = optional(string, "STABLE"),
     public_access = optional(object({
       enabled = optional(bool, false),
