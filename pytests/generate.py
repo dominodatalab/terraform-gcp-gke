78fa095067bdf6eb3dfd548180d08dff5a7e4051
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2025-06-23 15:35:55 -0700

    Python tests

---
+++ pytests/generate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sys
+import yaml
+from os import path
+from os.path import dirname, join, normpath
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+from ddlcloud_generator_gke import gke_subparser
+from ddlcloud_tf_base_schemas import TFBackendConfig, TFLocalBackend
+
+module_root = normpath(join(dirname(path.realpath(__file__)), ".."))
+
+def validate(module):
+    with TemporaryDirectory() as tmpdir:
+        module.backend = TFBackendConfig(
+            type="local",
+            config=TFLocalBackend(
+                path=path.join(tmpdir, "the.tfstate")
+            )
+        )
+        module.module.gke_cluster.source = module_root
+        with open(path.join(tmpdir, "main.tf.json"), "w") as f:
+            f.write(module.render_to_json())
+        run(["terraform", "init"], cwd=tmpdir, check=True)
+        run(["terraform", "validate"], cwd=tmpdir, check=True)
+
+def parse_args(test_args: list | None = None):
+    parser = argparse.ArgumentParser(prog="gke_test")
+    subparser = parser.add_subparsers(title="Commands", metavar="{command}")
+    common_args = argparse.ArgumentParser(add_help=False)
+    common_args.add_argument("--upgrade", help="Upgrade from existing file", action="store_true")
+    common_args.add_argument("--deploy-id", help="Name for deployment", required=True)
+    gke_subparser(subparser, [common_args]).set_defaults(command=True)
+
+    args = parser.parse_args(test_args)
+
+    if not getattr(args, "command", None):
+        parser.print_help()
+        sys.exit(0)
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    tf_module = args.generator(args, {})
+
+    print(yaml.safe_dump(tf_module.model_dump(by_alias=True)))
+
+if __name__ == "__main__":
+    main()
---
+++ pytests/test_generator.py
@@ -0,0 +1,49 @@
+import yaml
+from os import path
+from os import path
+
+from unittest import TestCase
+
+from ddlcloud_generator_gke import gke_subparser
+from ddlcloud_tf_base_schemas import TFBackendConfig, TFLocalBackend
+
+from .generate import parse_args, validate
+
+
+class TestGenerator(TestCase):
+    maxDiff = None
+    
+    def get_tfmodule(self, input_args: list | None = None):
+        input_args = input_args or []
+        args = parse_args(["gke", "--deploy-id", "test", *input_args])
+        return args.generator(args, {})
+    
+    def test_generate_gke_module(self):
+        with open("fixtures/defaults.yaml") as f:
+            defaults = yaml.safe_load(f)
+
+        with self.subTest("defaults"):
+            tf_module = self.get_tfmodule()
+            self.assertDictEqual(tf_module.model_dump(by_alias=True), defaults)
+            for module in tf_module.configs.values():
+                validate(module)
+
+        with self.subTest("overrides"):
+            tf_module = self.get_tfmodule(
+                [
+                    "--location",
+                    "some-location",
+                    "--module-version",
+                    "some-module-version",
+                    "--kubeconfig_path",
+                    "/path/to/kubeconfig",
+                    "--dev",
+                ]
+            )
+            gke_cluster = defaults["configs"]["main"]["module"]["gke_cluster"]
+            gke_cluster["gke"]["kubeconfig"]["path"] = "/path/to/kubeconfig"
+            gke_cluster["location"] = "some-location"
+            gke_cluster["source"] = "github.com/dominodatalab/terraform-gcp-gke?ref=some-module-version"
+            self.assertDictEqual(tf_module.model_dump(by_alias=True), defaults)
+            for module in tf_module.configs.values():
+                validate(module)
