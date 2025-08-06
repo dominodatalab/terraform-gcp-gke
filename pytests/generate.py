6cfd1c2068705fd531ac2612953fdb5d76895dcc
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2025-08-06 16:18:58 -0700

    Push more of the arg generation into ddlcloud

--- pytests/generate.py
+++ pytests/generate.py
@@ -28,11 +28,16 @@ def validate(module):
 def parse_args(test_args: list | None = None):
     parser = argparse.ArgumentParser(prog="gke_test")
     subparser = parser.add_subparsers(title="Commands", metavar="{command}")
-    common_args = argparse.ArgumentParser(add_help=False)
-    common_args.add_argument("--upgrade", help="Upgrade from existing file", action="store_true")
-    common_args.add_argument("--deploy-id", help="Name for deployment", required=True)
-    common_args.add_argument("--file", help="Load existing file")
-    GKEGenerator.subparser(subparser, [common_args]).set_defaults(command=True)
+    _gen_args_parser = subparser.add_parser("gke", help="gke Terraform Generator")
+    _gen_args_parser.add_argument("--upgrade", help="Upgrade from existing file", action="store_true")
+    _gen_args_parser.add_argument("--deploy-id", help="Name for deployment", required=True)
+    _gen_args_parser.add_argument("--file", help="Load existing file")
+    _gen_args_parser.add_argument(
+        "--kubeconfig-path", help="Override path for generated kubeconfig", default="kubeconfig"
+    )
+    _gen_args_parser.add_argument("--module-version", help="Version for terraform module", default="v3.1.3")
+    GKEGenerator.add_specific_args(_gen_args_parser)
+    _gen_args_parser.set_defaults(command=True)
 
     args = parser.parse_args(test_args)
 
