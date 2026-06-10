304f57fdc3022e2f7ac9d4a012178e354bd4a42f
ddl-retornam <56278673+ddl-retornam@users.noreply.github.com>
2026-06-10 09:59:18 -0700

    PLAT-9790:Update terraform provider
    Default k8s_version to 1.34

--- variables.tf
+++ variables.tf
@@ -136,7 +136,7 @@ variable "gke" {
   EOF
 
   type = object({
-    k8s_version     = optional(string, "1.34"),
+    k8s_version     = optional(string, "1.35"),
     release_channel = optional(string, "STABLE"),
     public_access = optional(object({
       enabled = optional(bool, false),
--- versions.tf
+++ versions.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0, < 6.0"
+      version = ">= 5.0, < 8.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1"
+      version = "~> 3.9"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.0, < 6.0"
+      version = ">= 5.0, < 8.0"
     }
   }
 }
